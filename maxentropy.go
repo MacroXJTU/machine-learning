f0ecf178fa1c97151926e77385431b730ef913a2
谢盼 <744647295@qq.com>
2019-09-04 11:16:33 +0800

    add test

--- maxentropy.go
+++ maxentropy.go
@@ -25,16 +25,16 @@ type MaxEntropy struct {
 	sync.Mutex
 	samples []*MnistSample //样本集合
 	test    []*MnistSample
-	w       map[int]float32    //最大熵模型的系数
-	Px      map[string]float32 //x出现的频率
-	Pxy     map[int]float32    //y出现的频率
-	EPxyf   map[int]float32    //特征函数关于经验分布的期望值
-	EPxf    map[int]float32    //特征函数关于模型和X经验分布的期望值
-	N       float32            //样本容量的大小的倒数
+	w       map[int]float64    //最大熵模型的系数
+	Px      map[string]float64 //x出现的频率
+	Pxy     map[int]float64    //y出现的频率
+	EPxyf   map[int]float64    //特征函数关于经验分布的期望值
+	EPxf    map[int]float64    //特征函数关于模型和X经验分布的期望值
+	N       float64            //样本容量的大小的倒数
 	n       int                //（x,y）的对数 pair，不是log
-	M       float32            //类似于学习率
+	M       float64            //类似于学习率
 	cvt     map[string]int     //（x,y）对和index之间的转换
-	fxyv    map[string]float32 //fxy的函数值
+	fxyv    map[string]float64 //fxy的函数值
 }
 
 //从文件中加载数据
@@ -92,12 +92,12 @@ func loadData() []*MnistSample {
 func CreateMaxEntropyModel() *MaxEntropy {
 	d := loadData()
 	Shuffle(d) //读取和打乱输入的样本数据
-	return (&MaxEntropy{samples: d[:len(d)-10000], test: d[len(d)-10000:]}).makeIndex().train(1000)
+	return (&MaxEntropy{samples: d[:len(d)-10000], test: d[len(d)-10000:]}).makeIndex()
 }
 
 //创建坐标转换数据
 func (m *MaxEntropy) makeIndex() *MaxEntropy {
-	m.fxyv = make(map[string]float32, 10*len(m.samples[0].Features)*2)
+	m.fxyv = make(map[string]float64, 10*len(m.samples[0].Features)*2)
 	for i := 0; i <= 9; i++ {
 		for j := 0; j < len(m.samples[0].Features); j++ {
 			m.fxyv[fmt.Sprintf("%d_%d:%d", j, 0, i)] = 0.0
@@ -150,7 +150,7 @@ func (m *MaxEntropy) id2xy(id int) (string, int) {
 
 //计算各维度特征出现的概率和联合概率
 func (m *MaxEntropy) calcPxyPx() *MaxEntropy {
-	m.Px, m.Pxy = make(map[string]float32), make(map[int]float32)
+	m.Px, m.Pxy = make(map[string]float64), make(map[int]float64)
 	for _, v := range m.samples {
 		for _, feature := range v.Features {
 			m.Pxy[m.xy2id(feature, v.Label)] += 1
@@ -163,7 +163,7 @@ func (m *MaxEntropy) calcPxyPx() *MaxEntropy {
 //计算特征函数关于经验分布的期望值
 func (m *MaxEntropy) calcEpxyf() *MaxEntropy {
 	if m.EPxyf == nil {
-		m.EPxyf = make(map[int]float32, m.n)
+		m.EPxyf = make(map[int]float64, m.n)
 	}
 	for i := 0; i < m.n; i++ {
 		m.EPxyf[i] = (m.Pxy[i]) * m.N
@@ -174,7 +174,7 @@ func (m *MaxEntropy) calcEpxyf() *MaxEntropy {
 //计算特征函数关于现有模型和经验分布的期望值
 func (m *MaxEntropy) calcEpxf() *MaxEntropy {
 	if m.EPxf == nil {
-		m.EPxf = make(map[int]float32, m.n)
+		m.EPxf = make(map[int]float64, m.n)
 	}
 	//这里是重新分配一段存储更快，还是逐个清除，待比较
 	//不过不要太沉迷微观上的优化
@@ -203,7 +203,7 @@ func (m *MaxEntropy) calcEpxf() *MaxEntropy {
 }
 
 //指定的f(x,y)函数计算过程,就是(x,y)有没有出现过
-func (m *MaxEntropy) fxy(x string, y int) float32 {
+func (m *MaxEntropy) fxy(x string, y int) float64 {
 	return m.fxyv[x+convert[y]]
 	/*
 		v, _ := m.EPxyf[m.xy2id(x, y)]
@@ -215,22 +215,22 @@ func (m *MaxEntropy) fxy(x string, y int) float32 {
 }
 
 type TPxy struct {
-	p float32
+	p float64
 	y int
 }
 
 //计算(x,y)出现的概率，预测的过程就是找(x,y)最大的那个y
 func (m *MaxEntropy) calcProb(features []string) []*TPxy {
 
-	p := make([]float32, 10)
-	totalP := float32(0.0) //做分母的概率总和
+	p := make([]float64, 10)
+	totalP := float64(0.0) //做分母的概率总和
 	for y := 0; y <= 9; y++ {
 		//这里是写死的代码，因为我们已经知道标签是0到9
 		p[y] = m.pxy(features, y).p
 		totalP += p[y]
 	}
 	/* 去掉一次没必要的循环
-	totalP := float32(0.0) //做分母的概率总和
+	totalP := float64(0.0) //做分母的概率总和
 	for _, v := range p {
 		totalP += v
 	}*/
@@ -245,7 +245,7 @@ func (m *MaxEntropy) calcProb(features []string) []*TPxy {
 
 //计算针对特定维度特征计算(x,y)概率
 func (m *MaxEntropy) pxy(features []string, y int) *TPxy {
-	r := float32(0.0)
+	r := float64(0.0)
 	//调用频率太高，考虑继续分拆加速
 	for _, x := range features {
 		//if m.fxy(x, y) {
@@ -254,7 +254,7 @@ func (m *MaxEntropy) pxy(features []string, y int) *TPxy {
 		//if 分支转成乘法
 	}
 	//考虑exp函数是否可以加速
-	return &TPxy{float32(math.Exp(float64(r))), y}
+	return &TPxy{float64(math.Exp(float64(r))), y}
 }
 
 //模型训练
@@ -270,12 +270,12 @@ func (m *MaxEntropy) train(maxIteration int) *MaxEntropy {
 
 	//设置变量值
 	//m.N用到的都是除法，弄成倒数，换成乘法
-	m.N, m.n, m.M = 1.0/float32(len(m.samples)), len(m.Pxy), 1.0/50000.0
+	m.N, m.n, m.M = 1.0/float64(len(m.samples)), len(m.Pxy), 1.0/50000.0
 	//计算EPxyf
 	m.calcEpxyf()
 	//迭代优化w
 	//分配和初始化全0的系数
-	m.w = make(map[int]float32, m.n) //其他系数在(x,y)不存在时f(x,y)=0，所以没有必要存在
+	m.w = make(map[int]float64, m.n) //其他系数在(x,y)不存在时f(x,y)=0，所以没有必要存在
 	startTs := time.Now().Unix()
 	for iter := 0; iter < maxIteration; iter++ {
 
@@ -283,12 +283,15 @@ func (m *MaxEntropy) train(maxIteration int) *MaxEntropy {
 		//耗掉了大部分时间，是优化的主要位置
 		m.calcEpxf()
 		for i := 0; i < m.n; i++ {
-			//m.w[i] += float32(1.0) / m.M * float32(math.Log(float64(m.EPxyf[i]/m.EPxf[i])))
-			m.w[i] += m.M * float32(math.Log(float64(m.EPxyf[i]/m.EPxf[i]))) //去掉一次除法运算
+			//m.w[i] += float64(1.0) / m.M * float64(math.Log(float64(m.EPxyf[i]/m.EPxf[i])))
+			m.w[i] += m.M * float64(math.Log(float64(m.EPxyf[i]/m.EPxf[i]))) //去掉一次除法运算
 		}
 		fmt.Printf("%d loop, last loop cost %d s.\n", iter+1, time.Now().Unix()-startTs)
 		startTs = time.Now().Unix()
-
+		//每10次迭代测试一次
+		if iter%10 == 0.0 {
+			m.Test()
+		}
 	}
 	return m
 }
@@ -297,7 +300,7 @@ func (m *MaxEntropy) train(maxIteration int) *MaxEntropy {
 func (m *MaxEntropy) Predict(feature []string) int {
 	//预测的过程就是在w给定情况下，Pw(y,x)取得最大值的y
 	pxy := m.calcProb(feature)
-	Y, P := 0, float32(0.0)
+	Y, P := 0, float64(0.0)
 	for _, v := range pxy {
 		if v.p >= P {
 			P = v.p
@@ -307,10 +310,8 @@ func (m *MaxEntropy) Predict(feature []string) int {
 	return Y
 }
 
-//最大熵模型的测试
-func TestMaxEntropy() {
-	fmt.Println("----------Test max entropy-------------------")
-	m := CreateMaxEntropyModel()
+//训练过程中进行测试
+func (m *MaxEntropy) Test() {
 	acc := 0
 	for _, v := range m.test {
 		p := m.Predict(v.Features)
@@ -319,5 +320,12 @@ func TestMaxEntropy() {
 			acc += 1
 		}
 	}
-	fmt.Printf("accuracy:=%f\n", float32(acc)/float32(len(m.test)))
+	fmt.Printf("accuracy:=%f\n", float64(acc)/float64(len(m.test)))
+}
+
+//最大熵模型的测试
+func TestMaxEntropy() {
+	fmt.Println("----------Test max entropy-------------------")
+	m := CreateMaxEntropyModel()
+	m.train(2000)
 }
